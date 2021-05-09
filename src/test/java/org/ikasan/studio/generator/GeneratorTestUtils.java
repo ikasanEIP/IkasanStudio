cfc727d28a1cf0d3f483c05001a911b3f4dce053
david <davidhilton68@gmail.com>
2021-05-09 02:06:26 +0100

    Track test failure

--- src/test/java/org/ikasan/studio/generator/GeneratorTestUtils.java
+++ src/test/java/org/ikasan/studio/generator/GeneratorTestUtils.java
@@ -16,6 +16,7 @@ public class GeneratorTestUtils  extends TestCase {
         String testFile = Context.FREEMARKER_TEMPLATE_PATH + expectedTestResultFileName;
         InputStream in = GeneratorTestUtils.class.getResourceAsStream(testFile);
         String expectedString = StringUtil.convertLineSeparators(IOUtils.toString(in, StandardCharsets.UTF_8));
+        System.out.println(expectedString);
         return expectedString;
     }
 
