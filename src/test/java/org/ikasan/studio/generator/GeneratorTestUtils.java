a5a91eab150ac68dd326c5f140089d7c424be795
david <davidhilton68@gmail.com>
2021-05-09 01:57:06 +0100

    tidy and fix tests

--- src/test/java/org/ikasan/studio/generator/GeneratorTestUtils.java
+++ src/test/java/org/ikasan/studio/generator/GeneratorTestUtils.java
@@ -1,19 +1,25 @@
 package org.ikasan.studio.generator;
 
 import com.intellij.openapi.util.text.StringUtil;
+import junit.framework.TestCase;
 import org.apache.commons.io.IOUtils;
 import org.ikasan.studio.Context;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-public class GeneratorTestUtils {
+public class GeneratorTestUtils  extends TestCase {
 
     public static String getExptectedFreemarkerOutputFromTestFile (String expectedTestResultFileName) throws IOException {
         String testFile = Context.FREEMARKER_TEMPLATE_PATH + expectedTestResultFileName;
         InputStream in = GeneratorTestUtils.class.getResourceAsStream(testFile);
         String expectedString = StringUtil.convertLineSeparators(IOUtils.toString(in, StandardCharsets.UTF_8));
         return expectedString;
+    }
+
+    @Test
+    public void test_tbc() throws IOException {
     }
 }
