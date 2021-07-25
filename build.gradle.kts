a9975b2e2d30bc81522a940272858ca1c35df297
david <davidhilton68@gmail.com>
2021-07-25 18:04:21 +0100

    Update plugin and image

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,36 +17,11 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.17.1"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
-//    id("java")
-//    // Kotlin support
-//    id("org.jetbrains.kotlin.jvm") version "1.5.10"
-//    // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-//    id("org.jetbrains.intellij") version "1.0"
-//    // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-//    id("org.jetbrains.changelog") version "0.6.2"
-//    // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-//    id("io.gitlab.arturbosch.detekt") version "1.17.1"
-//    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-//    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
-
-// Import variables from gradle.properties file
-val pluginGroup: String by project
-// `pluginName_` variable ends with `_` because of the collision with Kotlin magic getter in the `intellij` closure.
-// Read more about the issue: https://github.com/JetBrains/intellij-platform-plugin-template/issues/29
-val pluginName_: String by project
-val pluginVersion: String by project
-val pluginSinceBuild: String by project
-val pluginUntilBuild: String by project
-val pluginVerifierIdeVersions: String by project
-
-val platformType: String by project
-val platformVersion: String by project
-val platformPlugins: String by project
-val platformDownloadSources: String by project
 
 group = properties("pluginGroup")
 version = properties("pluginVersion")
+
 // Configure project's dependencies
 repositories {
     mavenCentral()
@@ -154,10 +129,4 @@ tasks {
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
-
-    // This seems quite brittle, to test, IJ requires to state the local path of where intellij community source code is installed.
-//    test {
-// //        systemProperty("idea.home.path", "/dev/ws/intellij-community-193.7288")
-//         systemProperty("idea.home.path", intellijRootDir().canonicalPath)
-//    }
 }
