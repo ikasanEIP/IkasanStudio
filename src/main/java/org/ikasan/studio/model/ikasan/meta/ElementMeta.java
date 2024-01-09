32985ef37bd6d75a81c13d379de7e08b33b886a7
davidhilton68 <davidhilton68@gmail.com>
2024-01-09 23:14:54 +0000

    Add support for Element Meta

---
+++ src/main/java/org/ikasan/studio/model/ikasan/meta/ElementMeta.java
@@ -0,0 +1,25 @@
+package org.ikasan.studio.model.ikasan.meta;
+
+import org.ikasan.studio.StudioUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Holds the meta data for all Ikasan elements
+ * enum is used as a convenient singleton implementation
+ */
+public enum ElementMeta implements Serializable {
+    INSTANCE;
+    private final List<Element> elements;
+
+    ElementMeta() {
+        elements = new ArrayList<>();
+        StudioUtils.nreadIkasanComponentProperties("");
+    }
+
+    public static final List<Element> getElementMeta() {
+        return INSTANCE.elements;
+    }
+}
