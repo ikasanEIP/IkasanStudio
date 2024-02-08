48f3a57fd63a034c043375d54950e3cab11a529a
davidhilton68 <davidhilton68@gmail.com>
2024-02-08 23:49:02 +0000

    deserialise Module with new technique, fix broken test.

--- src/main/java/org/ikasan/studio/io/ComponentIO.java
+++ src/main/java/org/ikasan/studio/io/ComponentIO.java
@@ -56,34 +56,6 @@ public class ComponentIO {
      * @return an IkasanBaseElement object representing the deserialized class
      * @throws StudioException  to wrap JsonProcessingException
      */
-//    public static IkasanBaseElement deserializeInstanceComponent(final String path, IkasanBaseElement objectToCreate) throws StudioException {
-//
-//        final InputStream inputStream = ComponentIO.class.getClassLoader().getResourceAsStream(path);
-//        if (inputStream == null) {
-//            throw new StudioException("The serialised data in [" + path + "] could not be loaded, check the path is correct");
-//        }
-//        final String jsonString = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
-//                .lines()
-//                .collect(Collectors.joining());
-//
-////        IkasanBaseElement ikasanBaseElement;
-//        Object ikasanBaseElement;
-//        try {
-////            ikasanBaseElement = objectToCreate.callBack(MAPPER, jsonString);
-//            ikasanBaseElement = MAPPER.readValue(jsonString, org.ikasan.studio.model.ikasan.instance.Module.class);
-//        } catch (JsonProcessingException e) {
-//            throw new StudioException("The serialised data in [" + path + "] could not be read due to" + e.getMessage(), e);
-//        }
-//        return (IkasanBaseElement)ikasanBaseElement;
-//    }
-
-    /*
-     * Deserialize the component at the given path.
-     * The component could be any of the child classes of IkasanBaseElement
-     * @param path to the file containing the JSON to deserializes
-     * @return an IkasanBaseElement object representing the deserialized class
-     * @throws StudioException  to wrap JsonProcessingException
-     */
     public static Module deserializeModuleInstance(final String path) throws StudioException {
 
         final InputStream inputStream = ComponentIO.class.getClassLoader().getResourceAsStream(path);
