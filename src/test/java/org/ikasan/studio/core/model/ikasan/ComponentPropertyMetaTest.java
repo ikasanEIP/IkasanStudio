1b524e253545017dab7f671175e5e4d75b756100
davidhilton68 <davidhilton68@gmail.com>
2025-04-29 21:16:26 +0100

    Improve regexp from FQN Types to option include prototype

--- src/test/java/org/ikasan/studio/core/model/ikasan/ComponentPropertyMetaTest.java
+++ src/test/java/org/ikasan/studio/core/model/ikasan/ComponentPropertyMetaTest.java
@@ -12,19 +12,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ComponentPropertyMetaTest {
     ComponentPropertyMeta componentPropertyMeta;
     public static final String CLASS_NAME_PATTERN = "^[A-Z_$][a-zA-Z\\d_$£]*$";
+    public static final String FULLY_QUALIFIED_CLASS_NAME_PATTERN = "([a-zA-Z_][\\w]*\\.)*([A-Z][\\w]*)(\\$[A-Z][\\w]*)*(<.*>)?";
     public static final String VALIDATION_MESSAGE = "Please provide a class name";
 
     @BeforeEach
     public void setup() {
+
+    }
+
+    @Test
+    public void test_class_name_pattern_matcher_is_populated_and_works() {
         componentPropertyMeta = ComponentPropertyMeta.builder()
                 .propertyName("1")
                 .validation(CLASS_NAME_PATTERN)
                 .validationMessage(VALIDATION_MESSAGE)
                 .build();
-    }
 
-    @Test
-    public void test_pattern_matcher_is_populated_and_works() {
         Pattern validationPattern = componentPropertyMeta.getValidationPattern();
         Assertions.assertNotNull(validationPattern);
         {
@@ -59,7 +62,65 @@ public class ComponentPropertyMetaTest {
                     () -> assertEquals(VALIDATION_MESSAGE, componentPropertyMeta.getValidationMessage())
             );
         }
-
     }
 
+    @Test
+    public void test_fully_qualified_class_name_pattern_matcher_is_populated_and_works() {
+        componentPropertyMeta = ComponentPropertyMeta.builder()
+                .propertyName("1")
+                .validation(FULLY_QUALIFIED_CLASS_NAME_PATTERN)
+                .validationMessage(VALIDATION_MESSAGE)
+                .build();
+
+        Pattern validationPattern = componentPropertyMeta.getValidationPattern();
+        Assertions.assertNotNull(validationPattern);
+        {
+            Matcher matcher = validationPattern.matcher("invalid class");
+            assertAll(
+                    "Check the module contains the expected values",
+                    () -> assertFalse(matcher.matches()),
+                    () -> assertEquals(VALIDATION_MESSAGE, componentPropertyMeta.getValidationMessage())
+            );
+        }
+        {
+            Matcher matcher = validationPattern.matcher("String");
+            assertAll(
+                    "Check the module contains the expected values",
+                    () -> assertTrue(matcher.matches()),
+                    () -> assertEquals(VALIDATION_MESSAGE, componentPropertyMeta.getValidationMessage())
+            );
+        }
+        {
+            Matcher matcher = validationPattern.matcher("java.util.List");
+            assertAll(
+                    "Check the module contains the expected values",
+                    () -> assertTrue(matcher.matches()),
+                    () -> assertEquals(VALIDATION_MESSAGE, componentPropertyMeta.getValidationMessage())
+            );
+        }
+        {
+            Matcher matcher = validationPattern.matcher("java.util.List<java.lang.String>");
+            assertAll(
+                    "Check the module contains the expected values",
+                    () -> assertTrue(matcher.matches()),
+                    () -> assertEquals(VALIDATION_MESSAGE, componentPropertyMeta.getValidationMessage())
+            );
+        }
+        {
+            Matcher matcher = validationPattern.matcher("2InValidClass");
+            assertAll(
+                    "Check the module contains the expected values",
+                    () -> assertFalse(matcher.matches()),
+                    () -> assertEquals(VALIDATION_MESSAGE, componentPropertyMeta.getValidationMessage())
+            );
+        }
+//        {
+//            Matcher matcher = validationPattern.matcher("inValidClass");
+//            assertAll(
+//                    "Check the module contains the expected values",
+//                    () -> assertFalse(matcher.matches()),
+//                    () -> assertEquals(VALIDATION_MESSAGE, componentPropertyMeta.getValidationMessage())
+//            );
+//        }
+    }
 }
